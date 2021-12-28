22b8d0f4c199d1d40950fdc49526b25501ad874f
Seo-A-Nam <42.4.senam@gmail.com>
2021-12-29 00:38:04 +0900

    Done : assignment4

---
+++ assignment5/assignment5.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class   stack
+{
+    public :
+        
+};
+int     main()
+{
+
+    return (0);
+}
+/*
+ * 템플릿? : https://blockdmask.tistory.com/43
+ * 
+ */
